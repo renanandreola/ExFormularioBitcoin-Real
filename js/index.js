3255122a91e1caeacca8383cc74eab6af76c62d5
Aluno <renanandreolla@outlook.com>
2019-08-22 21:39:59 -0300

    teste

---
+++ js/index.js
@@ -0,0 +1,7 @@
+//1) Criar um formulário que converta os valores de real para
+//bitcoin e bitcoin para real, esse formulário deve ter um
+//radio button com duas opções "Converter para real" e
+//"Converter para bitcoin" e um input text para o usuário
+//digitar o valor que gostaria de converter. No final deverá
+//exibir o resultado da conversão.
+//Serviço JSON: https://www.mercadobitcoin.net/api/BTC/ticker/
